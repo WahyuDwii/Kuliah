16b6b3add54df8a82f4cc13fb09bbf49d4cbea5e
Wahyu Dwi Yulianto <wahyu.dwi8742@gmail.com>
2024-08-26 14:53:58 +0700

    wahyu

---
+++ Praktikum PBO/wahyuproject1.java
@@ -0,0 +1,9 @@
+public class wahyuproject1 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Wahyu Dwi Yulianto");
+        System.out.println("NPM : 2340506056");
+        System.out.println("Alamat : Jl.Raya Bandongan ");
+        System.out.println("No_HP : 089525146655");
+    }
+}
+
