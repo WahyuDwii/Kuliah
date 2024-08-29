0c06056ceb6dc46edec9a7fc27b00532273f0882
WahyuDwii <wahyu.dwi8742@gmail.com>
2024-08-29 20:45:13 +0700

    wahyu

---
+++ Praktikum PBO/Project 1/Wahyu_project1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wahyu_project1;
+
+/**
+ *
+ * @author W Λ H Y U D W I
+ */
+public class Wahyu_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Wahyu Dwi Yulianto");
+        System.out.println("NPM : 2340506056");
+        System.out.println("Alamat : Jl.Raya Bandongan ");
+        System.out.println("No_HP : 089525146655");
+    }
+}
+
+
